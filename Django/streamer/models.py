decce3e3c4e454132fa6b28c7ab50ea5a25229af
Takashi Uno <euno.eng@gmail.com>
2022-10-25 08:10:42 +0900

    add : crypto model

--- Django/streamer/models.py
+++ Django/streamer/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 from django_boost.models.mixins import LogicalDeletionMixin
+from django_cryptography.fields import encrypt
 
 
 class AnimeRoom(LogicalDeletionMixin):
@@ -14,7 +15,7 @@ class AnimeRoom(LogicalDeletionMixin):
 
 class AnimeUser(LogicalDeletionMixin):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user_name = models.CharField(default="user", max_length=20)
+    user_name = encrypt(models.CharField(default="user", max_length=20))
     room_id = models.ForeignKey(
         AnimeRoom, on_delete=models.CASCADE, related_name="inroom"
     )
@@ -31,7 +32,7 @@ class ReactionType(models.TextChoices):
     middle_finger = "MF", "middle_finger"
     smile = "S", "smile"
     thumbs_up = "TU", "thumbs_up"
-    fav = "F", "fav"
+    fav = "F", "favorite"
 
 
 class AnimeReaction(LogicalDeletionMixin):
@@ -45,13 +46,3 @@ class AnimeRoomHistory(models.Model):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     room_id = models.ForeignKey(AnimeRoom, on_delete=models.CASCADE)
     type = models.CharField(max_length=20)
-
-
-class TVRoom(LogicalDeletionMixin):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    room_id = models.UUIDField()
-    host_user_id = models.UUIDField()
-    num_people = models.PositiveSmallIntegerField(default=0)
-    max_num_people = models.PositiveSmallIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
